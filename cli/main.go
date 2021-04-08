924ae2fcf0b9d9933b90b7665d75b4738e0197e9
jan-okok <yuanjian.cheng@hoo.com>
2021-04-08 09:52:59 +0800

    main:新增程序锁

--- cli/main.go
+++ cli/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"github.com/pkg/errors"
+	"github.com/prometheus/tsdb/fileutil"
 	"github/bsc-task/cli/command"
 	"github/bsc-task/log"
+	"path/filepath"
 )
 
 func main() {
+	instanceLock := lockDataDir()
+	defer lockRelease(instanceLock)
 	log.Init()
 	command.Execute()
+}
+
+func lockDataDir() fileutil.Releaser {
+	lock, _, err := fileutil.Flock(filepath.Join("LOCK"))
+	if err != nil {
+		panic(errors.Wrapf(err, "启动失败：程序正在执行或上次执行未正常结束").Error())
+	}
+	return lock
+}
+
+func lockRelease(instanceLock fileutil.Releaser) {
+	err := instanceLock.Release()
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/fortytw2/leaktest v1.3.0 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.4 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/prometheus/tsdb v0.7.1
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.1.3
